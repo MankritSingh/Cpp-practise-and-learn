ea2762efb92b6113fc1fa0c86c954330b1b0687f
MankritSingh <67284593+MankritSingh@users.noreply.github.com>
2021-04-05 14:45:10 +0530

    Add files via upload

---
+++ helloworld.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+
+int main() {
+	int num1;
+	double num2;
+	std::cout<<"Hello World\n Enter a number\n";
+	std::cin>>num1;
+	std::cout<<"Enter number 2";
+	std::cin>>num2;
+	std::cout<<"Number1 is "<<num1<<"num2 is"<<num2;
+	return 0;
+}
